10d515bff703beda0d3b0a9af86ea4bb89333bfb
sukhyunil@naver.com <sukhyunil@naver.com>
2021-04-17 18:05:04 +0900

    sdadss

--- src/main/java/com/bedmil/todolist/JpaMappingTest.java
+++ src/main/java/com/bedmil/todolist/JpaMappingTest.java
@@ -55,4 +55,47 @@ public class JpaMappingTest {
         assertThat(savedTodo.getContent()).isEqualTo(todo.getContent());
 	}
 	
-}
+	   @Test
+	    public void test_save() {
+	        // GIVEN
+	        Todo todo = Todo.builder()
+	                        .content("내용1")
+	                        .isComplete(true)
+	                        .createdDateTime(LocalDateTime.now())
+	                        .build();
+	        
+	        // WHEN
+	        Todo savedTodo = todoRepository.save(todo);
+	        System.out.println("=========================");
+	        System.out.println(savedTodo.getId());
+	        System.out.println(savedTodo.getContent());
+	        System.out.println(savedTodo.getIsComplete());
+	        System.out.println(savedTodo.getCreatedDateTime());
+	        System.out.println("=========================");
+	        
+	        // THEN
+	        assertThat(savedTodo.getId()).isGreaterThan(0);
+	        assertThat(savedTodo.getContent()).isEqualTo("내용1");
+	        assertThat(savedTodo.getIsComplete()).isEqualTo(true);
+	    }
+	    
+	    @Test
+	    public void test_delete() {
+	        // GIVEN
+	        Todo todo = getSaved();
+	        System.out.println("=========================");
+	        System.out.println(todo.getId());
+	        System.out.println(todo.getContent());
+	        System.out.println(todo.getIsComplete());
+	        System.out.println(todo.getCreatedDateTime());
+	        System.out.println("=========================");
+	        Long id = todo.getId();
+	        
+	        // WHEN
+	        todoRepository.deleteById(id);
+	        
+	        // THEN
+	        assertThat(entityManager.find(Todo.class, id)).isNull();
+	    }
+	}
+
--- src/main/java/com/bedmil/todolist/TodolistApplication.java
+++ src/main/java/com/bedmil/todolist/TodolistApplication.java
@@ -3,6 +3,8 @@ package com.bedmil.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class TodolistApplication {
 
@@ -10,4 +12,16 @@ public class TodolistApplication {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
 
+	// 데이터 생성 테스트
+//	@Bean
+//	public CommandLineRunner runner(TodoRepository todoRepository) throws Exception {
+//		return (arg) -> {
+//			IntStream.rangeClosed(1, 10).forEach(index -> todoRepository.save(Todo.builder()
+//					.content("오늘 할 일" + index)
+//					.createdDateTime(LocalDateTime.now())
+//					.isComplete(false)
+//					.build())
+//					);
+//		};
+//	}
 }
